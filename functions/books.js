fecdaaa9cf323fe48c7c73da1dfd975dddda7dfc
megane42 <h.kzm.gm@gmail.com>
2019-02-10 17:53:01 +0900

    use isbn as document id in order to implement unique constraint

--- functions/books.js
+++ functions/books.js
@@ -14,8 +14,10 @@ app.use(cors({ origin: true }));
 // actions
 
 const add_book = (req, res) => {
-  return isbn.resolve(req.params.isbn).then((book) => {
-    return firestore.collection('books').add(book)
+  return isbn.resolve(
+    req.params.isbn
+  ).then((book) => {
+    return firestore.collection('books').doc(req.params.isbn).set(book)
   }).then(() => {
     return res.status(200).send({status: "ok"})
   }).catch(() => {
